0412c8096a48ead81320bc4c890d954c1c207d71
Vera Tikhonova <veratihonova95@gmail.com>
2022-08-16 01:04:45 +0500

    fixed js header-menu

--- source/js/modules/header-menu.js
+++ source/js/modules/header-menu.js
@@ -1,21 +1,42 @@
 const header = document.querySelector('.header');
 const menu = document.querySelector('.header__wrapper');
 const toggle = document.querySelector('.header__toggle');
+const content = document.querySelector('.header__content');
+
+const openMenu = () => {
+  menu.classList.remove('header__wrapper--closed');
+  menu.classList.add('header__wrapper--opened');
+  header.style.position = 'fixed';
+  header.style.padding = '0';
+  document.addEventListener('click', outsideClickListener);
+};
+
+const closeMenu = () => {
+  menu.classList.add('header__wrapper--closed');
+  menu.classList.remove('header__wrapper--opened');
+  header.style.position = 'absolute';
+  header.style.padding = '0 20px';
+  document.removeEventListener('click', outsideClickListener);
+};
+
+function isVisible(elem) {
+  return !!elem && !!(elem.offsetWidth || elem.offsetHeight || elem.getClientRects().length);
+}
+
+const outsideClickListener = (event) => {
+  if (!content.contains(event.target) && isVisible(content)) {
+    closeMenu();
+  }
+};
 
 const headerMenu = () => {
   header.style.position = 'absolute';
   menu.classList.add('header__wrapper--closed');
   toggle.addEventListener('click', function () {
     if (menu.classList.contains('header__wrapper--closed')) {
-      menu.classList.remove('header__wrapper--closed');
-      menu.classList.add('header__wrapper--opened');
-      header.style.position = 'fixed';
-      header.style.padding = '0';
+      openMenu();
     } else {
-      menu.classList.add('header__wrapper--closed');
-      menu.classList.remove('header__wrapper--opened');
-      header.style.position = 'absolute';
-      header.style.padding = '0 20px';
+      closeMenu();
     }
   });
 };
